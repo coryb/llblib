80f1b11432e7cd6a094bfbe662119dfe0551cbee
coryb <cbennett@netflix.com>
2023-03-30 19:11:05 -0700

    add IncludePatterns helper for llb.CopyOptions

--- run.go
+++ run.go
@@ -17,12 +17,31 @@ func IgnoreCache() llb.RunOption {
 	return llb.AddEnv("LLBLIB_IGNORE_CACHE", identity.NewID())
 }
 
+type copyOptionFunc func(*llb.CopyInfo)
+
+func (f copyOptionFunc) SetCopyOption(ci *llb.CopyInfo) {
+	f(ci)
+}
+
+func IncludePatterns(p []string) llb.CopyOption {
+	return copyOptionFunc(func(ci *llb.CopyInfo) {
+		ci.IncludePatterns = p
+	})
+}
+
 type NullOption struct{}
 
 func (NullOption) SetConstraintsOption(c *llb.Constraints) {}
-func (NullOption) SetRunOption(*llb.ExecInfo)              {}
-func (NullOption) SetLocalOption(*llb.LocalInfo)           {}
+func (NullOption) SetCopyOption(*llb.CopyInfo)             {}
+func (NullOption) SetGitOption(*llb.GitInfo)               {}
 func (NullOption) SetHTTPOption(*llb.HTTPInfo)             {}
 func (NullOption) SetImageOption(*llb.ImageInfo)           {}
-func (NullOption) SetGitOption(*llb.GitInfo)               {}
+func (NullOption) SetLocalOption(*llb.LocalInfo)           {}
+func (NullOption) SetMkdirOption(*llb.MkdirInfo)           {}
+func (NullOption) SetMkfileOption(*llb.MkfileInfo)         {}
 func (NullOption) SetOCILayoutOption(*llb.OCILayoutInfo)   {}
+func (NullOption) SetRmOption(*llb.RmInfo)                 {}
+func (NullOption) SetRunOption(*llb.ExecInfo)              {}
+func (NullOption) SetSSHOption(*llb.SSHInfo)               {}
+func (NullOption) SetSecretOption(*llb.SecretInfo)         {}
+func (NullOption) SetTmpfsOption(*llb.TmpfsInfo)           {}
